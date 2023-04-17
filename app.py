5bf04b201ac7cd40a8c946b68ed227a9a7141e6f
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-17 09:46:10 +0200

    Ad flash messages with Jinja to improve user experience. Use the different categories for the flash message. Add tests for flash message.

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import os
-from flask import Flask, escape, render_template, request
+from flask import Flask, escape, render_template, request, session, redirect, url_for, flash
 from pydantic import BaseModel, validator, ValidationError
 
 
+# --------------
+# Helper Classes
+# --------------
+
 class StockModel(BaseModel):
+    """Class for parsing new stock data from a form."""
     stock_symbol: str
     number_of_shares: int
     purchase_price: float
@@ -12,7 +17,7 @@ class StockModel(BaseModel):
     def stock_symbol_check(cls, value):
         if not value.isalpha() or len(value) > 5:
             raise ValueError('Stock symbol must be 1-5 characters')
-        return value.upper
+        return value.upper()
 
 
 app = Flask(__name__)
@@ -25,13 +30,13 @@ def index():
 
 @app.route('/about')
 def about():
-    # return render_template('about.html', company_name='TestDrive.io')
-    return render_template('about.html')
+    flash('Thanks for learning about this site!', 'info')
+    return render_template('about.html', company_name='TestDriven.io')
 
 
 @app.route('/stocks/')
-def stocks():
-    return '<h2>Stock List...</h2>'
+def list_stocks():
+    return render_template('stocks.html')
 
 
 @app.route('/hello/<message>')
@@ -47,6 +52,7 @@ def display_blog_post(post_id):
 @app.route('/add_stock', methods=['GET', 'POST'])
 def add_stock():
     if request.method == 'POST':
+        # Print the form data to the console
         for key, value in request.form.items():
             print(f'{key}: {value}')
 
@@ -57,6 +63,14 @@ def add_stock():
                 purchase_price=request.form['purchase_price']
             )
             print(stock_data)
+
+            # Save the form data to the session object
+            session['stock_symbol'] = stock_data.stock_symbol
+            session['number_of_shares'] = stock_data.number_of_shares
+            session['purchase_price'] = stock_data.purchase_price
+            flash(f'Added new stock ({stock_data.stock_symbol})!', 'success')
+
+            return redirect(url_for('list_stocks'))
         except ValidationError as e:
             print(e)
 
--- tests/functional/test_stocks.py
+++ tests/functional/test_stocks.py
@@ -33,6 +33,22 @@ def test_about_page():
         assert b'Course developed by TestDriven.io' in response.data
 
 
+def test_about_page_has_flashed_message():
+    """
+    GIVEN a Flask application
+    WHEN the '/about' page is requested (GET)
+    THEN check the response is valid and flash message is visible
+    """
+    with app.test_client() as client:
+        response = client.get('/about')
+        assert response.status_code == 200
+        assert b'Flask Stock Portfolio App' in response.data
+        assert b'About' in response.data
+        assert b'This application is built using the Flask web framework.' in response.data
+        assert b'Course developed by TestDriven.io' in response.data
+        assert b'Thanks for learning about this site!' in response.data
+
+
 def test_get_add_stock_page():
     """
     GIVEN a Flask application
@@ -69,3 +85,26 @@ def test_post_add_stock_page():
         assert b'AAPL' in response.data
         assert b'23' in response.data
         assert b'432.17' in response.data
+
+
+def test_post_add_stock_page_and_get_flashed_message():
+    """
+    GIVEN a Flask application
+    WHEN the '/add_stock' page is posted to (POST)
+    THEN check the user is redirected to the '/list_stocks' page and sees the flashed message
+    """
+    with app.test_client() as client:
+        response = client.post('/add_stock', data={
+            'stock_symbol': 'AAPL',
+            'number_of_shares': '23',
+            'purchase_price': '432.17'
+        }, follow_redirects=True)
+        assert response.status_code == 200
+        assert b'List of Stocks' in response.data
+        assert b'Stock Symbol' in response.data
+        assert b'Number of Shares' in response.data
+        assert b'Purchase Price' in response.data
+        assert b'AAPL' in response.data
+        assert b'23' in response.data
+        assert b'432.17' in response.data
+        assert b'Added new stock (AAPL)!' in response.data
