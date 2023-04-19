d32e071a534a38c3ce916578ed30db2441282e52
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-19 12:03:18 +0200

    Add fixture. Expand test_client to initialize database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,14 @@
 import pytest
 from project import create_app
 from flask import current_app  # <--- proxy
+from project.models import Stock
+from project import database
+
+
+@pytest.fixture(scope='module')
+def new_stock():
+    stock = Stock('AAPL', '16', '406.78')
+    return stock
 
 
 @pytest.fixture(scope='module')
@@ -11,6 +19,15 @@ def test_client():
     # Create a test client using the Flask application configured for testing
     with flask_app.test_client() as testing_client:
         with flask_app.app_context():
-            current_app.logger.info('In the test_client() fixture...')
+            # Establish an application context before accessing the logger and database
+            flask_app.logger.info('Creating database tables in test_client fixture...')
 
-        yield testing_client
+            # Create the database and the database table(s)
+            database.create_all()
+
+        yield testing_client  # This is where the testing happens!
+
+        # After the test functions complete, control is returned to the fixture
+        # so that the database can be cleared
+        with flask_app.app_context():
+            database.drop_all()
