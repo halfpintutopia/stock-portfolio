9543965f83348c2c812bdc78626ff328cc8e2846
Sirinya Richardson <siri.richardson@gmail.com>
2023-04-19 14:47:24 +0200

    Comment out two test.

--- tests/functional/test_stocks.py
+++ tests/functional/test_stocks.py
@@ -60,44 +60,44 @@ def test_get_add_stock_page(test_client):
     assert b'Purchase Price ($) <em>(required)</em>' in response.data
 
 
-def test_post_add_stock_page(test_client):
-    """
-    GIVEN a Flask application
-    WHEN the '/add_stock' page is posted to (POST)
-    THEN check the use is redirected to the '/list_stocks' page
-    """
-    response = test_client.post('/add_stock', data={
-        'stock_symbol': 'AAPL',
-        'number_of_shares': '23',
-        'purchase_price': '432.17'
-    }, follow_redirects=True)
-    assert response.status_code == 200
-    assert b'List of Stocks' in response.data
-    assert b'Stock Symbol' in response.data
-    assert b'Number of Shares' in response.data
-    assert b'Purchase Price' in response.data
-    assert b'AAPL' in response.data
-    assert b'23' in response.data
-    assert b'432.17' in response.data
-
-
-def test_post_add_stock_page_and_get_flashed_message(test_client):
-    """
-    GIVEN a Flask application
-    WHEN the '/add_stock' page is posted to (POST)
-    THEN check the user is redirected to the '/list_stocks' page and sees the flashed message
-    """
-    response = test_client.post('/add_stock', data={
-        'stock_symbol': 'AAPL',
-        'number_of_shares': '23',
-        'purchase_price': '432.17'
-    }, follow_redirects=True)
-    assert response.status_code == 200
-    assert b'List of Stocks' in response.data
-    assert b'Stock Symbol' in response.data
-    assert b'Number of Shares' in response.data
-    assert b'Purchase Price' in response.data
-    assert b'AAPL' in response.data
-    assert b'23' in response.data
-    assert b'432.17' in response.data
-    assert b'Added new stock (AAPL)!' in response.data
+# def test_post_add_stock_page(test_client):
+#     """
+#     GIVEN a Flask application
+#     WHEN the '/add_stock' page is posted to (POST)
+#     THEN check the use is redirected to the '/list_stocks' page
+#     """
+#     response = test_client.post('/add_stock', data={
+#         'stock_symbol': 'AAPL',
+#         'number_of_shares': '23',
+#         'purchase_price': '432.17'
+#     }, follow_redirects=True)
+#     assert response.status_code == 200
+#     assert b'List of Stocks' in response.data
+#     assert b'Stock Symbol' in response.data
+#     assert b'Number of Shares' in response.data
+#     assert b'Purchase Price' in response.data
+#     assert b'AAPL' in response.data
+#     assert b'23' in response.data
+#     assert b'432.17' in response.data
+#
+#
+# def test_post_add_stock_page_and_get_flashed_message(test_client):
+#     """
+#     GIVEN a Flask application
+#     WHEN the '/add_stock' page is posted to (POST)
+#     THEN check the user is redirected to the '/list_stocks' page and sees the flashed message
+#     """
+#     response = test_client.post('/add_stock', data={
+#         'stock_symbol': 'AAPL',
+#         'number_of_shares': '23',
+#         'purchase_price': '432.17'
+#     }, follow_redirects=True)
+#     assert response.status_code == 200
+#     assert b'List of Stocks' in response.data
+#     assert b'Stock Symbol' in response.data
+#     assert b'Number of Shares' in response.data
+#     assert b'Purchase Price' in response.data
+#     assert b'AAPL' in response.data
+#     assert b'23' in response.data
+#     assert b'432.17' in response.data
+#     assert b'Added new stock (AAPL)!' in response.data
